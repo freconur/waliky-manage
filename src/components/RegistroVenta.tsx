20604e2a562af54e4e3cfb427fabb57613a8c8aa
frecodev <freconur@gmail.com>
2023-02-06 23:26:14 -0500

    corrigiendo los warning de los inputs

--- src/components/RegistroVenta.tsx
+++ src/components/RegistroVenta.tsx
@@ -4,68 +4,62 @@ import { initialStateProducts, searchIdReducer } from "../reducer/searchId.reduc
 import { InputValueVentas, SearchById } from '../types'
 
 interface StateRegistroVentas {
-    id: SearchById
+	id: SearchById
 }
 
 const RegistroVenta = () => {
-    const [inputIdValue, setInputIdValue] = useState<InputValueVentas>({
-        id: '',
-        cantidad: 0
-    })
-   
-    const [state, dispatch] = useReducer(searchIdReducer, initialStateProducts)
-    const { product, pathProduct } = state
+	const [inputIdValue, setInputIdValue] = useState<InputValueVentas>({
+		id: '',
+		cantidad: 0
+	})
 
-    useEffect(() => {
+	const [state, dispatch] = useReducer(searchIdReducer, initialStateProducts)
+	const { product, pathProduct } = state
 
-    }, [product,pathProduct])
-    console.log('pathProduct',pathProduct)
-    const onChangeIdFormVentas = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setInputIdValue({
-            ...inputIdValue,
-            [e.target.name]: e.target.value
-        })
-
-    }
-    console.log('inputIdValue', inputIdValue)
-    const onSubmitCheckId = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-        e.preventDefault()
-
-        getProductById(dispatch, inputIdValue)
-    }
-    // const onSubmitFormVentas = (e: React.FormEvent<HTMLFormElement>) => {
-        const onSubmitFormVentas = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-        e.preventDefault()
-        if(product.stock){
-            if(product.stock > inputIdValue.cantidad) {
-                const newStock:number =  product.stock - inputIdValue.cantidad
-                updateStockProduct(dispatch, pathProduct,inputIdValue, newStock)
-            }else {
-                console.log('estas ingresando una mayor cantidad al stock disponible')
-            }
-        }
-    }
-    return (
-
-        <>
-            <h1>Holis</h1>
-            {/* <form className="form-ventas" onSubmit={onSubmitFormVentas}> */}
-            <form className="form-ventas">
-                <div>
-                    <input onChange={onChangeIdFormVentas} type="text" placeholder="id" name="id" />
-                    <button onClick={onSubmitCheckId}>✅</button>
-                </div>
-                <input type="text" value={product.name} placeholder="nombre" name="name" />
-                <input type="text" value={product.price} placeholder="precio" name="price" />
-                <input type="number" value={product.stock} placeholder="stock" name="stock" />
-                <input onChange={onChangeIdFormVentas} type="number" placeholder="cantidad" name="cantidad" />
-                {product.stock
-                && <input type="text" value="activo" placeholder="estado" name="state" />}
-                {/* <input type="boolean" value={product.state} placeholder="estado" name="state" /> */}
-                <button onClick={onSubmitFormVentas}>registrar</button>
-            </form>
-        </>
-    )
+	useEffect(() => {
+	}, [product, pathProduct])
+	const onChangeIdFormVentas = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setInputIdValue({
+			...inputIdValue,
+			[e.target.name]: e.target.value
+		})
+	}
+	const onSubmitCheckId = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+		e.preventDefault()
+		getProductById(dispatch, inputIdValue)
+	}
+	// const onSubmitFormVentas = (e: React.FormEvent<HTMLFormElement>) => {
+	const onSubmitFormVentas = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+		e.preventDefault()
+		if (product.stock) {
+			if (product.stock > inputIdValue.cantidad) {
+				const newStock: number = product.stock - inputIdValue.cantidad
+				updateStockProduct(pathProduct, inputIdValue, newStock)
+			} else {
+				console.log('estas ingresando una mayor cantidad al stock disponible')
+			}
+		}
+	}
+	return (
+		<>
+			<h1>Holis</h1>
+			{/* <form className="form-ventas" onSubmit={onSubmitFormVentas}> */}
+			<form className="form-ventas">
+				<div>
+					<input onChange={onChangeIdFormVentas} type="text" placeholder="id" name="id" />
+					<button onClick={onSubmitCheckId}>✅</button>
+				</div>
+				<input type="text" disabled placeholder={`${product.name}`} name="name" />
+				<input type="text" disabled placeholder={`${product.price}`} name="price" />
+				<input type="number" disabled placeholder={`${product.stock}`} name="stock" />
+				<input onChange={onChangeIdFormVentas} type="number" placeholder="ingresa cantidad" name="cantidad" />
+				{product.stock
+					&& <input type="text" disabled value="activo" placeholder="estado" name="state" />}
+				{/* <input type="boolean" value={product.state} placeholder="estado" name="state" /> */}
+				<button onClick={onSubmitFormVentas}>registrar</button>
+			</form>
+		</>
+	)
 }
 
 export { RegistroVenta }
