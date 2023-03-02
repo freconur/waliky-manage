aca0bfc5f72f9c6dacb44930b7a542b7f3415834
frecodev <freconur@gmail.com>
2023-03-01 19:32:34 -0500

    agregando detalles de hover

--- src/components/ListSideBar.tsx
+++ src/components/ListSideBar.tsx
@@ -31,23 +31,23 @@ const ListSidebar = ({ openSidebar, pruebita }: Props) => {
 					<span className={`text-base flex-1 ml-2 font-semibold ${!openSidebar && "hidden"}`}>registro de venta</span>
 				</Link>
 			</li>
-			<li  onClick={() => pruebita(!openSidebar)} className=" text-sm items-center gap-x-4 cursor-pointer overflow-hidden  rounded-md mt-2 capitalize whitespace-nowrap">
+			<li  onClick={() => pruebita(!openSidebar)} className=" text-sm items-center gap-x-4 cursor-pointer overflow-hidden   mt-2 capitalize whitespace-nowrap">
 				{/* <Link to="/registro-de-productos" className="w-56 p-2"> */}
-				<div onClick={() => setProductActive(!productActive)} className="flex overflow-hidden duration-300 p-2 rounded-md hover:bg-red-200 text-gray-300 hover:text-gray-500">
+				<div onClick={() => setProductActive(!productActive)} className={`flex  duration-300 p-2 rounded-lg hover:bg-red-200 text-gray-300 hover:text-gray-500 ${productActive && "rounded-none rounded-t-lg bg-red-200 text-gray-500"}`}>
 					<RiArchiveDrawerFill className="text-2xl block float-left " />
 					<span className={` text-base flex-1 ml-2 font-semibold   ${!openSidebar && "hidden"}`}>productos</span>
 					<RiArrowDownSLine className={`text-[30px] duration-300  ${!openSidebar && "hidden" } ${productActive && "rotate-180"}`} />
 				</div>
 				{/* </Link> */}
 				{productActive && openSidebar && 
-					<ul className={`duration-1000  bg-white rounded-lg scale-100 overflow-hidden `}>
-						<li className="text-gray-400  p-3 text-md flex items-center gap-x-4 cursor-pointer capitalize   hover:bg-red-200 hover:text-gray-500 whitespace-nowrap duration-300 font-semibold">
-							<Link to="registro-de-productos" className="ml-10">
+					<ul className={`duration-1000  bg-white rounded-lg scale-100 overflow-hidden ${productActive && "rounded-none rounded-b-lg"} `}>
+						<li className="text-gray-400 text-md flex items-center gap-x-4 cursor-pointer capitalize   hover:bg-red-100 hover:text-gray-500 whitespace-nowrap duration-300 font-semibold">
+							<Link to="registro-de-productos" className="ml-10 py-2 w-full">
 							mis productos
 							</Link>
 						</li>
-						<li className="text-gray-400  p-3 text-md flex items-center gap-x-4 cursor-pointer capitalize   hover:bg-red-200 hover:text-gray-500 whitespace-nowrap duration-300 font-semibold">
-							<Link to="/agregar-producto" className="ml-10">
+						<li className="text-gray-400 text-md flex items-center gap-x-4 cursor-pointer capitalize   hover:bg-red-100 hover:text-gray-500 whitespace-nowrap duration-300 font-semibold">
+							<Link to="/agregar-producto" className="ml-10 py-2 w-full">
 							agregar producto
 							</Link>
 						</li>
