6633662e49a1efccc2a6148ae20af5b2449eee15
frecodev <freconur@gmail.com>
2023-03-01 19:41:03 -0500

    fixeando margin de select de filtro de categorias de productos

--- src/components/OptionFilterCategories.tsx
+++ src/components/OptionFilterCategories.tsx
@@ -15,7 +15,7 @@ const OptionFilterCategories = ({onChangeCategories}: PropsCategories) => {
 		},[])
     return(
         <>
-        <div className="mb-2 mt-5 flex justify-between">
+        <div className="mb-2 mt-5 mr-5 flex justify-between">
 				<label className="text-gray-500 capitalize font-semibold mr-2">filtrar por:</label>
 				{/* <select onChange={onChangeValueSelect} className="px-3 text-gray-400 capitalize font-semibold py-1 rounded-lg shadow-md w-32" name="" id=""> */}
 				<select onChange={onChangeCategories} className="px-3 text-gray-400 capitalize font-semibold py-1 rounded-lg shadow-md w-32" name="" id="">
