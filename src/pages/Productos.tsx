b444f48d4dacce6bd055ac1abbf0f783b324ba00
frecodev <freconur@gmail.com>
2023-03-06 15:09:27 -0500

    agregnado locader spinner

--- src/pages/Productos.tsx
+++ src/pages/Productos.tsx
@@ -5,6 +5,8 @@ import { Product } from "../types";
 import { CopyToClipboard } from 'react-copy-to-clipboard'
 import { OptionFilterCategories } from "../components/OptionFilterCategories";
 import { ProductModal } from "../modal/ProductModal";
+import { RiLoader4Line } from "react-icons/ri";
+
 import { Link } from "react-router-dom";
 
 
@@ -27,11 +29,11 @@ const Productos = () => {
   useEffect(() => {
     getAllProducts(dispatch)
   }, [])
-  function ModalState (): void {
+  function ModalState(): void {
     setModalActive(!modalActive)
   }
   const onChangeCategories = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    dispatch({type:"filterCategory", payload:allProducts, payload2: e.target.value, payload3:copyAllProducts})
+    dispatch({ type: "filterCategory", payload: allProducts, payload2: e.target.value, payload3: copyAllProducts })
     setCurrentPage(0)
   }
   const nextPage = () => {
@@ -44,7 +46,7 @@ const Productos = () => {
       setCurrentPage(currentPage - 5)
     }
   }
-  const onSearchChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+  const onSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault()
     setSearch(e.target.value)
   }
@@ -53,55 +55,66 @@ const Productos = () => {
     <div className="max-cz:ml-5 ml-10 mt-5 w-auto">
       {modalActive && <ProductModal modalState={ModalState} itemInfo={itemInfo} />}
       <h1 className="capitalize text-3xl font-bold text-gray-600">tus productos</h1>
-      <OptionFilterCategories onChangeCategories={onChangeCategories}/>
-      {/* <OptionFilterCategories /> */}
-      <div className="mt-2 mr-5">
-        <p className="capitalize text-xl text-gray-500 mb-2">buscar</p>
-        <input className="border-2 w-full p-2 rounded-lg" onClick={() => setCurrentPage(0)} onChange={onSearchChange} value={search} type="text" />
-      </div>
+      <OptionFilterCategories onChangeCategories={onChangeCategories} />
+      {
+        allProducts.length > 0
+          ?
+          <div>
 
-      <div className="overflow-auto rounded-lg shadow mt-2 mr-5">
-        <table className="w-full  overflow-auto">
-          <thead className="bg-gray-50 border-b-2 border-gray-200">
-            <tr>
-              <th className="p-3 capitalize text-gray-500 w-10  text-sm font-semibold tracking-wide text-left">id</th>
-              <th className="p-3 capitalize text-gray-500 w-[1024px] text-sm font-semibold tracking-wide text-left">nombre</th>
-              <th className="p-3 capitalize text-gray-500 w-10 text-sm font-semibold tracking-wide text-left">precio</th>
-              <th className="p-3 capitalize text-gray-500 w-10 max-cz:hidden text-sm font-semibold tracking-wide text-left">marca</th>
-              <th className="p-3 capitalize text-gray-500 text-sm font-semibold tracking-wide text-left">imagen</th>
-            </tr>
-          </thead>
-          <tbody className="divide-y divide-gray-100 ">
-            {filterProducts().map((item, index) => {
-              return (
-                <tr onClick={() => {setModalActive(!modalActive); item && setItemInfo(item)}} key={item.id} className="duration-1000 relative cursor-pointer">
-                  {/* <Link to="/"> */}
-                  <td className=" cursor-pointer duration-900  bg-white p-3 capitalize text-gray-400 text-md ">
-                    <div className="duration-500 rounded-full font-bold hover:underline hover:bg-blue-200 text-center bg-blue-400 text-white h-[25px] w-[25px]">
-                      <CopyToClipboard text={`${item.id}`}>
-                        <span>
-                          {index + 1}
-                        </span>
-                      </CopyToClipboard>
-                    </div>
-                  </td>
-                  <td className="p-3 capitalize text-gray-400  w-10 bg-white text-md">{item.name}</td>
-                  <td className="p-3 capitalize text-gray-400  bg-white text-md">S/{item.price}</td>
-                  <td className="p-3 capitalize text-gray-400 max-cz:hidden bg-white text-md">{item.marca}</td>
-                  <td className="p-3 capitalize text-gray-400  bg-white text-md">
-                    <img className="w-[50px] h-[50px]" src={item.image} alt={item.name} />
-                  </td>
-                  {/* </Link> */}
-                </tr>
-              )
-            })}
-          </tbody>
-        </table>
-      </div>
-      <div className="mt-3">
-        <button onClick={previewPage} className="p-2 mr-5 rounded bg-orange-500 duration-300 hover:bg-orange-400 font-semibold drop-shadow-xl text-white cursor-pointer">anterior</button>
-        <button onClick={nextPage} className="p-2 mr-5 rounded bg-orange-500 duration-300 hover:bg-orange-400 font-semibold drop-shadow-xl text-white cursor-pointer">siguiente</button>
-      </div>
+            <div className="mt-2 mr-5">
+              <p className="capitalize text-xl text-gray-500 mb-2">buscar</p>
+              <input className="border-2 w-full p-2 rounded-lg" onClick={() => setCurrentPage(0)} onChange={onSearchChange} value={search} type="text" />
+            </div>
+            <div className={`overflow-auto rounded-lg shadow mt-2 mr-5`}>
+              <table className="w-full  overflow-auto">
+                <thead className="bg-gray-50 border-b-2 border-gray-200">
+                  <tr>
+                    <th className="p-3 capitalize text-gray-500 w-10  text-sm font-semibold tracking-wide text-left">id</th>
+                    <th className="p-3 capitalize text-gray-500 w-[1024px] text-sm font-semibold tracking-wide text-left">nombre</th>
+                    <th className="p-3 capitalize text-gray-500 w-10 text-sm font-semibold tracking-wide text-left">precio</th>
+                    <th className="p-3 capitalize text-gray-500 w-10 max-cz:hidden text-sm font-semibold tracking-wide text-left">marca</th>
+                    <th className="p-3 capitalize text-gray-500 text-sm font-semibold tracking-wide text-left">imagen</th>
+                  </tr>
+                </thead>
+                <tbody className="divide-y divide-gray-100 ">
+                  {filterProducts().map((item, index) => {
+                    return (
+                      <tr onClick={() => { setModalActive(!modalActive); item && setItemInfo(item) }} key={item.id} className="duration-1000 relative cursor-pointer">
+                        {/* <Link to="/"> */}
+                        <td className=" cursor-pointer duration-900  bg-white p-3 capitalize text-gray-400 text-md ">
+                          <div className="duration-500 rounded-full font-bold hover:underline hover:bg-blue-200 text-center bg-blue-400 text-white h-[25px] w-[25px]">
+                            <CopyToClipboard text={`${item.id}`}>
+                              <span>
+                                {index + 1}
+                              </span>
+                            </CopyToClipboard>
+                          </div>
+                        </td>
+                        <td className="p-3 capitalize text-gray-400  w-10 bg-white text-md">{item.name}</td>
+                        <td className="p-3 capitalize text-gray-400  bg-white text-md">S/{item.price}</td>
+                        <td className="p-3 capitalize text-gray-400 max-cz:hidden bg-white text-md">{item.marca}</td>
+                        <td className="p-3 capitalize text-gray-400  bg-white text-md">
+                          <img className="w-[50px] h-[50px]" src={item.image} alt={item.name} />
+                        </td>
+                      </tr>
+                    )
+                  })}
+                </tbody>
+              </table>
+              <div className="mt-3">
+                <button onClick={previewPage} className="p-2 mr-5 rounded bg-orange-500 duration-300 hover:bg-orange-400 font-semibold drop-shadow-xl text-white cursor-pointer">anterior</button>
+                <button onClick={nextPage} className="p-2 mr-5 rounded bg-orange-500 duration-300 hover:bg-orange-400 font-semibold drop-shadow-xl text-white cursor-pointer">siguiente</button>
+              </div>
+            </div>
+          </div>
+          :
+          <div className="w-[340px] flex justify-center mt-5 ">
+            <div className="items-center">
+              <RiLoader4Line className="animate-spin text-3xl text-blue-500 m-auto " />
+              <p className="text-gray-400">cargando productos...</p>
+            </div>
+          </div>
+      }
     </div>
   )
 
